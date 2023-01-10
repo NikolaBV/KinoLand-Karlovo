035f0d642d15b48881ceadd633b8e19dc8163c94
Nikola Valkov <bulgarianmapper64@gmail.com>
2023-01-10 15:18:30 +0200

    Add project files.

---
+++ KinoLand Karlovo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace KinoLand_Karlovo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
